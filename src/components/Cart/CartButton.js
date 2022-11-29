c3131ef384ebc58ef48198e92189f3ce2b086733
Hossam1104 <hossammohamed1104@gmail.com>
2022-11-29 20:59:43 +0200

    Simple-Redux-Cart

---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,22 @@
+import { useDispatch, useSelector } from "react-redux";
+import { uiActions } from "../../store/ui-slice";
+import classes from "./CartButton.module.css";
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const cardQuantity = useSelector((state) => state.cart.totalQuantity);
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
+  return (
+    <button className={classes.button} onClick={toggleCartHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>{cardQuantity}</span>
+    </button>
+  );
+};
+
+export default CartButton;
